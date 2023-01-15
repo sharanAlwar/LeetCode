28994521c8cd96bd64efe3a01aea35ce5550e6e2
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-01-16 00:38:59 +0530

    Create #2194.java

---
+++ #2194.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> cellsInRange(String s) {
+        List<String> answerList = new ArrayList<>();
+        int startLetter, startNumber, endLetter, endNumber;
+        char[] charArray = s.toCharArray();
+        startLetter = charArray[0];
+        startNumber = Character.getNumericValue(charArray[1]);
+        endLetter = charArray[3];
+        endNumber = Character.getNumericValue(charArray[4]);
+        for(int i = startLetter; i <= endLetter; i++) {
+            String letter = String.valueOf((char) i);
+            for (int j = startNumber; j <= endNumber; j++) {
+                answerList.add(letter + j);
+            }
+        }
+        return answerList;
+    }
+}
